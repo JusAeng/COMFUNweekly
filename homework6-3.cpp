6393ec5e21a9d0ebf82717138d77f2406a05827f
JusAeng <69185572+JusAeng@users.noreply.github.com>
2020-09-15 01:30:30 +0700

    carPark

---
+++ homework6-3.cpp
@@ -0,0 +1,34 @@
+#define _CRT_SECURE_NO_WARNINGS
+//CarPark//
+#include <stdio.h>
+int main()
+{
+	int numb,m,n,i,j;
+	int x[100]={},y[100]={},posi[100][100]={};
+	printf("Input size of car-park (MxN): ");
+	scanf("%d %d",&m,&n);
+	printf("Cars in park : ");
+	scanf("%d",&numb);
+	for(i=1;i<=numb;i++)
+	{
+		printf("Position of cars NO.%d (x,y): ",i);
+		scanf("%d %d",&x[i],&y[i]);
+		posi[x[i]][y[i]]=1;
+	}
+	for(i=1;i<=m;i++)
+	{
+		for(j=1;j<=n;j++)
+		{
+			if(posi[i][j]==1)
+			{
+				printf("x");
+			}
+			else
+			{
+				printf("_");
+			}
+		}
+		printf("\n");
+	}
+	return 0;
+}
