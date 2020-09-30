60ea186ea0ef0c0e7c8992a91311247ce7afa9d9
JusAeng <69185572+JusAeng@users.noreply.github.com>
2020-09-30 22:09:21 +0700

    rotationMatrix

---
+++ homework8-1.cpp
@@ -0,0 +1,30 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+//Counterclockwise_RotationMatrix
+int main()
+{
+	int m,n,i,j,box[50][50],boxr[50][50];
+	printf("enter size (MxN) : ");
+	scanf("%d %d",&m,&n);
+	for(i=1;i<=m;i++)
+	{
+		for(j=1;j<=n;j++)
+		{
+			scanf("%d",&box[i-1][j-1]);
+		}
+	}
+	
+	//Rotate
+	printf("Rotate!!\n");
+	for(i=1;i<=n;i++)
+	{
+		for(j=1;j<=m;j++)
+		{
+			printf("%d ",box[j-1][n-i]);
+		}
+		printf("\n");
+	}
+	
+	
+	return 0;
+}
